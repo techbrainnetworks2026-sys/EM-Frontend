cea1dd7fad282f4962811fca71da860f03e872e4
Shashankan-PKS <pksshashankan2003@gmail.com>
2026-02-09 21:03:42 +0530

    layout changes

--- src/components/manager/components/Navbar.jsx
+++ src/components/manager/components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar(){
 
     return (
         <div>
-            <AppBar elevation = {0} sx={{width : isMobile ? (sidebarOpen ? "300px" : "0px") : (sidebarOpen ? "300px" : "80px"), left: sidebarOpen ? "300px" : "80px", padding : "15px", background : "rgb(8, 15, 37)" }}>
+            <AppBar elevation = {0} sx={{width : isMobile ? (sidebarOpen ? "300px" : "0px") : (sidebarOpen ? "300px" : "80px"), left: isMobile && (sidebarOpen ? "300px" : "80px"), padding : "15px", background : "rgb(8, 15, 37)" }}>
                 <Box sx={{ display : "flex", flexDirection : "row", justifyContent : "space-between", alignItems : "center"}}>
                     <Box >
                         <IconButton onClick={handleSidebarOpen}>
