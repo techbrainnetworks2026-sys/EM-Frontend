42766c615c8bf4b1b96d99cf240bd6ddf16982e0
Shashankan-PKS <pksshashankan2003@gmail.com>
2026-02-09 21:25:02 +0530

    layout changes

--- src/components/manager/components/Sidebar.jsx
+++ src/components/manager/components/Sidebar.jsx
@@ -15,7 +15,7 @@ function Sidebar(){
     const { sidebarOpen, setSidebarOpen, isMobile } = useAppContext();
 
     return (
-        <div style={{ width : isMobile ? (sidebarOpen ? "300px" : "0px") : (sidebarOpen ? "300px" : "80px"), height : "100dvh",position: "fixed",top : 0, left : 0, zIndex: 1400, border : "white", background : "rgb(8, 15, 37)",boxShadow: isMobile && sidebarOpen ? "0 0 15px rgba(0,0,0,0.5)" : "none", overflow: "hidden",}}>
+        <div style={{ width : isMobile ? (sidebarOpen ? "300px" : "0px") : (sidebarOpen ? "300px" : "80px"), height : "100dvh",position: "fixed",top : 0, left : 0, zIndex: 1500, border : "white", background : "rgb(8, 15, 37)",boxShadow: isMobile && sidebarOpen ? "0 0 15px rgba(0,0,0,0.5)" : "none", overflow: "hidden",}}>
             <div style={{display : "flex", alignItems : "center", justifyContent : "center", height : "80px", padding : "10px", columnGap : "10px", textAlign : "center"}}>
                 {sidebarOpen && 
                     <>
@@ -192,7 +192,7 @@ function Sidebar(){
                     left: 0,
                     width: "100vw",
                     height: "100vh",
-                    background: "rgba(0,0,0,0.4)",
+                    background: "rgba(0,0,0,0.1)",
                     zIndex: 1300
                     }}
                 />
